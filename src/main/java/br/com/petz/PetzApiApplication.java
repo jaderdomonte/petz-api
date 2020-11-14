ab0c40c5bc88270cb43e1f43817e1417b266f3de
jaderdomonte <30699505+jaderdomonte@users.noreply.github.com>
2020-11-14 16:07:32 -0300

    Update

--- src/main/java/br/com/petz/PetzApiApplication.java
+++ src/main/java/br/com/petz/PetzApiApplication.java
@@ -2,12 +2,18 @@ package br.com.petz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetzApiApplication {
+public class PetzApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetzApiApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PetzApiApplication.class);
+    }
 }
